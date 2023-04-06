e2b180a19230f7b697f9320d8b28be8998cf191f
egu7088 <128982766+egu7088@users.noreply.github.com>
2023-04-06 22:12:15 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flash import Flask
+from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
@@ -9,6 +9,6 @@ def main():
 @app.route('/how are you')
 def hello():
     return 'I am good, how about you?'
-    
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
